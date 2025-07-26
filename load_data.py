2f656ee30cae81c232433b19f7ff84b2a37ee348
daakshayani-05 <akuladaakshayani@gmail.com>
2025-07-26 10:49:52 +0530

    Delete load_data.py

--- load_data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import sqlite3
-conn = sqlite3.connect('ecommerce.db')
-df = pd.read_csv('products.csv')
-df.to_sql('products',conn,if_exists='replace',index=False)
-conn.close()
